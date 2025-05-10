ea2eaf2fffe042e8ee689741887eae7392e35a1d
rishav76dev <rjrishav037@gmail.com>
2025-05-10 23:18:37 +0530

    started graphql server

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { ApolloServer } = require("@apollo/server")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const { expressMiddleware } = require('@apollo/server/express4');
+
+async function startServer(){
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean
+            }
+            type Query {
+                getTodos: [Todo]
+            }`,
+        resolvers: {}
+    });
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    await server.start()
+
+    app.use("/graphql", expressMiddleware(server));
+    app.listen(8000,() => console.log('server started at 8000'))
+}
+
+startServer()
